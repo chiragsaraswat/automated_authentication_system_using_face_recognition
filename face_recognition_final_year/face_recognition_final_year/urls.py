1310a03ec257b0a69cca86045ffda7b44c4d9561
ripyipman <csaraswat87@gmail.com>
2021-01-04 13:43:15 +0530

    Home Page Created, Templates and Static files Configured

--- face_recognition_final_year/face_recognition_final_year/urls.py
+++ face_recognition_final_year/face_recognition_final_year/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from face_recognizer_app import views as face_recog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',face_recog_views.index,name='index'),
+
+    path('face_recognizer',include('face_recognizer_app.urls')),
 ]
--- face_recognition_final_year/face_recognizer_app/views.py
+++ face_recognition_final_year/face_recognizer_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'face_recognizer_app/index.html')
+# def home(request):
+#     return render(request, 'face_recognizer_app/home.html')
